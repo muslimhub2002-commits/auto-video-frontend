17a75367afa85d61554177dcf74557373c2e49b6
akmgmerakmgmer <ahmedever80@gmail.com>
2026-01-07 15:46:14 +0200

    New

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve = config.resolve ?? {};
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      tailwindcss: path.resolve(__dirname, "node_modules/tailwindcss/index.css"),
+      "tw-animate-css": path.resolve(
+        __dirname,
+        "node_modules/tw-animate-css/dist/tw-animate.css",
+      ),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
